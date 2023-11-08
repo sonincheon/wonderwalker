c1859d19df9f4bf1d4bc690268e9eb9c8c9d8cf0
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2023-11-09 00:19:04 +0900

    11/09 선영(벡엔드 연결)

--- src/main/java/wonderwalker/project/mini/controller/TravelController.java
+++ src/main/java/wonderwalker/project/mini/controller/TravelController.java
@@ -1,5 +1,41 @@
 package wonderwalker.project.mini.controller;
 
-public class TravelController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import wonderwalker.project.mini.dao.GoodsDAO;
+import wonderwalker.project.mini.dao.TravelDAO;
+import wonderwalker.project.mini.vo.GoodsVO;
+import wonderwalker.project.mini.vo.TravelVO;
 
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+// 3000번포트로 넘어갔다가 8011번포트로 넘어가면 에러인데 이걸 풀기위해 CrossOrigin으로 선언하여 풀어줌
+@RestController
+// restful-API ??? 컨트롤러
+@RequestMapping("/DiyPage")
+// 그룹을 위한 Mapping
+public class TravelController {
+    @GetMapping("Diyview")
+    //조회정보 가져올떈 리퀘어파람으로 id값 뺴오기
+    public ResponseEntity<List<TravelVO>> TravelInfo(@RequestParam String world){
+
+        System.out.println(" world :  " + world);
+        TravelDAO dao =new TravelDAO(); //로그인
+        List<TravelVO> list = dao.TravelInfo(world);
+        //맴버 리스트 리턴해줌
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("DiyWrite")
+    //조회정보 가져올떈 리퀘어파람으로 id값 뺴오기
+    public ResponseEntity<List<TravelVO>> TravelContent(@RequestParam String itemCode){
+
+        System.out.println("itemcode: "+itemCode);
+        TravelDAO dao =new TravelDAO(); //로그인
+        List<TravelVO> list = dao.TravelContent(itemCode);
+        //맴버 리스트 리턴해줌
+        return new ResponseEntity<>(list,HttpStatus.OK);
+    }
 }
